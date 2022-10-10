a3bc5d1d3f4e074d63a992fc4e7b0a1f6980cddd
ELBARHMI <elbarhmi.oussama@gmail.com>
2022-10-10 12:03:21 +0200

    First-commit

---
+++ src/main/java/fr/exercice/HelloWorld.java
@@ -0,0 +1,15 @@
+package fr.exercice;
+
+public class HelloWorld {
+
+
+	/**
+	 * 
+	 * Afficher à l'écran hello Maven
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Maven");
+	}
+
+}
